47caa01d406622ce34aa388be0f007607a0176ec
DutcH <zachareidonguah@gmail.com>
2026-05-07 13:10:58 -0700

    Initial Commit

---
+++ Calendy/CalendyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendyApp.swift
+//  Calendy
+//
+//  Created by Caden Erwin on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CalendyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendy
+//
+//  Created by Caden Erwin on 5/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
